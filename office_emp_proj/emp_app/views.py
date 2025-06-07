3746e51adb9d4829fffa5c4b6550191aa20d472e
tejeshwini18 <tejeshwini18@gmail.com>
2025-06-07 12:55:44 +0530

    Improved UI

--- office_emp_proj/emp_app/views.py
+++ office_emp_proj/emp_app/views.py
@@ -31,7 +31,13 @@ def add_emp(request):
         new_emp.save()
         return HttpResponse('Employee added Successfully')
     elif request.method=='GET':
-        return render(request, 'add_emp.html')
+        departments = Department.objects.all()
+        roles = Role.objects.all()
+        context = {
+            'departments': departments,
+            'roles': roles
+        }
+        return render(request, 'add_emp.html', context)
     else:
         return HttpResponse("An Exception Occured! Employee Has Not Been Added")
 
